d6183363fa391a8bca165e5e6aae4ba43099c7c1
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-08 22:41:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-let MinCost = 0;
-function mincost(arr)
-{ 
+function mincost(arr) {
+
+	// start
+	if (arr.length==1) {
+		return arr[0];
+	}
+	if (arr.length>1) {
+let MinCostPrice = 0;
+function mincost1(arr){ 
 //write your code here
 // return the min cost
 // base condition
 if(arr.length==1){
-return MinCost;
+return MinCostPrice;
 }
 else{
 	let CostArray = [];
 	SortedArray = arr.sort((a,b)=>a-b); // Increasing order
-	CostArray[0] = SortedArray[0]+SortedArray[1]; // 5 ,9 
-	MinCost+=CostArray[0];
+	CostArray[0] = SortedArray[0]+SortedArray[1]; // 5 ,9 15
+	MinCostPrice+=CostArray[0]; // 14+15=29
 	for(i=1;i<SortedArray.length-1;i++){
     CostArray[i] = SortedArray[i+1]; // 5,4,6, 9,6
 	}
-	return mincost(CostArray);
+	return mincost1(CostArray);
 }
 }
-console.log(mincost(arr));
+console.log(mincost1(arr));
   
+}
 }
 module.exports=mincost;
